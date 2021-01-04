38e6b505bbcf8a8514b129e0b2ab03aa0841b4af
Felix Biemüller <biemueller@sim.tu-darmstadt.de>
2021-01-04 17:02:10 +0100

    Update minimum functions

--- hector_stability_metrics/include/hector_stability_metrics/math/minimum.h
+++ hector_stability_metrics/include/hector_stability_metrics/math/minimum.h
@@ -28,7 +28,7 @@ Scalar exponentialWeighting(const std::vector<Scalar>& values, const Scalar& a, 
 template <typename Scalar>
 Scalar standardMinimum(const std::vector<Scalar>& values)
 {
-  Scalar min_value = values[0];
+  Scalar min_value = std::numeric_limits<double>::max();
 
   for (int i = 1; i < values.size(); i++)
   {
@@ -38,16 +38,29 @@ Scalar standardMinimum(const std::vector<Scalar>& values)
 }
 
 template <typename Scalar>
-Scalar exponentialWeighting(const std::vector<Scalar>& values, const Scalar& a, const Scalar& b, const Scalar& c)
+Scalar exponentialWeighting(const std::vector<Scalar>& values, const Scalar& a, const Scalar& b)
 {
   Scalar value = 0;
 
   for (int i = 0; i < values.size(); i++)
   {
-    value += std::exp(-(b * values[i] + c));
+    value += exp(-(b * values[i]));
   }
   return value * a / values.size();
 }
+
+template <typename Scalar>
+Scalar logarithmicExponentialWeighting(const std::vector<Scalar>& values, const Scalar& a, const Scalar& b)
+{
+  Scalar value = 0;
+
+  for (int i = 0; i < values.size(); i++)
+  {
+    value += exp(-(b * values[i]));
+  }
+  return log(value) * a / values.size();
+}
+
 }  // namespace math
 }  // namespace hector_stability_metrics
 #endif  // HECTOR_STABILITY_METRICS_MINIMUM_FUNCTIONS_H

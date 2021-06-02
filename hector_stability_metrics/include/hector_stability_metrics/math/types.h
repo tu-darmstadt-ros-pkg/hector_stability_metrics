521916aa34033d4b8869508a89df125814e079cc
Felix Biemüller <biemueller@sim.tu-darmstadt.de>
2021-06-02 09:59:30 +0200

    Fix errors caused by the include path of Eigen

--- hector_stability_metrics/include/hector_stability_metrics/math/types.h
+++ hector_stability_metrics/include/hector_stability_metrics/math/types.h
@@ -4,51 +4,51 @@
 #ifndef HECTOR_STABILITY_METRICS_TYPES_H
 #define HECTOR_STABILITY_METRICS_TYPES_H
 
-#include <Eigen/Core>
-#include <Eigen/StdVector>
+#include <eigen3/Eigen/Core>
+#include <eigen3/Eigen/StdVector>
 #include <vector>
 
 namespace hector_stability_metrics
 {
 namespace math
 {
-template<typename Scalar>
+template <typename Scalar>
 using Matrix3 = Eigen::Matrix<Scalar, 3, 3>;
 using Matrix3f = Matrix3<float>;
 using Matrix3d = Matrix3<double>;
 
-template<typename Scalar>
+template <typename Scalar>
 using Vector2 = Eigen::Matrix<Scalar, 2, 1>;
 using Vector2f = Vector2<float>;
 using Vector2d = Vector2<double>;
-template<typename Scalar>
+template <typename Scalar>
 using Vector2List = std::vector<Vector2<Scalar>, Eigen::aligned_allocator<Vector2<Scalar> > >;
 using Vector2fList = Vector2List<float>;
 using Vector2dList = Vector2List<double>;
 
-template<typename Scalar>
+template <typename Scalar>
 using Vector3 = Eigen::Matrix<Scalar, 3, 1>;
 using Vector3f = Vector3<float>;
 using Vector3d = Vector3<double>;
-template<typename Scalar>
+template <typename Scalar>
 using Vector3List = std::vector<Vector3<Scalar> >;
 using Vector3fList = Vector3List<float>;
 using Vector3dList = Vector3List<double>;
 
-template<typename Scalar>
+template <typename Scalar>
 using VectorX = Eigen::Matrix<Scalar, Eigen::Dynamic, 1>;
 using VectorXf = VectorX<float>;
 using VectorXd = VectorX<double>;
-template<typename Scalar>
+template <typename Scalar>
 using VectorXList = std::vector<VectorX<Scalar> >;
 using VectorXfList = VectorXList<float>;
 using VectorXdList = VectorXList<double>;
 
-template<typename Scalar>
+template <typename Scalar>
 using Isometry3 = Eigen::Transform<Scalar, 3, Eigen::Isometry>;
 using Isometry3f = Isometry3<float>;
 using Isometry3d = Isometry3<double>;
-}
+}  // namespace math
 }  // namespace hector_stability_metrics
 
 #endif  // HECTOR_STABILITY_METRICS_TYPES_H
